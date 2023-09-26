7aef8ed09443c92c0a7be07db80a9c16987498d6
Moderocky <admin@moderocky.com>
2023-09-26 11:00:10 +0100

    Add current file expression.

---
+++ src/main/java/org/skriptlang/skript_io/elements/expressions/ExprCurrentFile.java
@@ -0,0 +1,101 @@
+package org.skriptlang.skript_io.elements.expressions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import ch.njol.skript.lang.*;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript_io.elements.effects.SecAccessFile;
+import org.skriptlang.skript_io.utility.FileController;
+
+import java.net.URI;
+
+@Name("Current File")
+@Description("The currently-open file inside a file reading/editing section.")
+@Examples({
+    "create a new file ./test.txt:",
+    "\tadd \"hello\" to the file"
+})
+@Since("1.0.0")
+public class ExprCurrentFile extends SimpleExpression<Object> {
+    
+    static {
+        Skript.registerExpression(ExprCurrentFile.class, Object.class, ExpressionType.SIMPLE,
+            "[the] [(current|open)] file"
+        );
+    }
+    
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        SkriptEvent skriptEvent = this.getParser().getCurrentSkriptEvent();
+        if (!this.getParser().isCurrentSection(
+            SecAccessFile.class) && !(skriptEvent instanceof SectionSkriptEvent && ((SectionSkriptEvent) skriptEvent).isSection(
+            SecAccessFile.class))) {
+            Skript.error("You can't use '" + parseResult.expr + "' outside a file access section.");
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    protected URI @NotNull [] get(@NotNull Event event) {
+        final FileController controller = FileController.currentSection(event);
+        if (controller == null) return new URI[0];
+        return new URI[]{controller.getPath()};
+    }
+    
+    @Override
+    public Class<?> @NotNull [] acceptChange(Changer.@NotNull ChangeMode mode) {
+        return switch (mode) {
+            case SET, ADD -> CollectionUtils.array(String.class);
+            default -> CollectionUtils.array();
+        };
+    }
+    
+    @Override
+    public void change(@NotNull Event event, Object @Nullable [] delta, Changer.@NotNull ChangeMode mode) {
+        final FileController controller = FileController.currentSection(event);
+        if (controller == null) return;
+        switch (mode) {
+            case ADD:
+                if (delta == null) break;
+                for (final Object thing : delta) {
+                    if (thing == null) continue;
+                    controller.append(String.valueOf(thing));
+                }
+                break;
+            case SET:
+                if (delta == null || delta.length < 1) break;
+                final Object thing = delta[0];
+                if (thing == null) controller.clear();
+                else controller.write(String.valueOf(thing));
+                break;
+            case RESET:
+                controller.clear();
+        }
+    }
+    
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+    
+    @Override
+    public @NotNull Class<URI> getReturnType() {
+        return URI.class;
+    }
+    
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "the file";
+    }
+    
+}
