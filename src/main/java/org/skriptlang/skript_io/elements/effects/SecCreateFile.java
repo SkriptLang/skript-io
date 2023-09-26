4fc97624dc06c02e188c8c680aa23bd52ca7c181
Moderocky <admin@moderocky.com>
2023-09-26 11:00:02 +0100

    Add file creation section.

---
+++ src/main/java/org/skriptlang/skript_io/elements/effects/SecCreateFile.java
@@ -0,0 +1,73 @@
+package org.skriptlang.skript_io.elements.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.config.SectionNode;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.TriggerItem;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript_io.SkriptIO;
+import org.skriptlang.skript_io.utility.FileController;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+
+public class SecCreateFile extends SecAccessFile {
+    
+    static {
+        Skript.registerSection(SecCreateFile.class,
+            "(create|make) [a] [new] file [at] %path%"
+        );
+    }
+    
+    private Expression<URI> pathExpression;
+    
+    @Override
+    public boolean init(Expression<?> @NotNull [] expressions, int matchedPattern, @NotNull Kleenean kleenean, SkriptParser.@NotNull ParseResult result, @Nullable SectionNode sectionNode, @Nullable List<TriggerItem> list) {
+        this.pathExpression = (Expression<URI>) expressions[0];
+        if (this.hasSection()) {
+            assert sectionNode != null;
+            this.loadOptionalCode(sectionNode);
+            if (last != null) last.setNext(null);
+        }
+        return true;
+    }
+    
+    @Override
+    protected @Nullable TriggerItem walk(Event event) {
+        final URI uri = pathExpression.getSingle(event);
+        if (uri == null) return this.walk(event, false);
+        final File file = SkriptIO.file(uri);
+        if (file == null) return this.walk(event, false);
+        if (file.exists() && !file.isFile()) return this.walk(event, false);
+        try {
+            if (file.getParentFile() != null) file.getParentFile().mkdirs();
+            file.createNewFile();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        if (!file.exists() || !file.isFile()) return this.walk(event, false);
+        TriggerItem walk = null;
+        try (final FileController controller = FileController.getController(file)) {
+            FileController.push(event, controller);
+            walk = this.walk(event, true);
+        } catch (IOException ex) {
+            SkriptIO.error(ex);
+        } finally {
+            FileController.pop(event);
+            if (walk == null) walk = this.walk(event, false);
+        }
+        return walk;
+    }
+    
+    @Override
+    public @NotNull String toString(@Nullable Event event, boolean debug) {
+        return "create file " + pathExpression.toString(event, debug);
+    }
+    
+}
