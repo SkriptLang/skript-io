ece9c089371d0825e3200b3082ffa5f30dcf8681
Moderocky <admin@moderocky.com>
2023-09-27 09:44:29 +0100

    Export methods to access section.

--- src/main/java/org/skriptlang/skript_io/elements/effects/SecAccessFile.java
+++ src/main/java/org/skriptlang/skript_io/elements/effects/SecAccessFile.java
@@ -1,6 +1,36 @@
 package org.skriptlang.skript_io.elements.effects;
 
+import ch.njol.skript.config.SectionNode;
 import ch.njol.skript.lang.EffectSection;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.TriggerItem;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.net.URI;
+import java.util.List;
 
 public abstract class SecAccessFile extends EffectSection {
+    
+    protected Expression<URI> pathExpression;
+    
+    @Override
+    public boolean init(Expression<?> @NotNull [] expressions, int matchedPattern, @NotNull Kleenean kleenean, SkriptParser.@NotNull ParseResult result, @Nullable SectionNode sectionNode, @Nullable List<TriggerItem> list) {
+        this.pathExpression = (Expression<URI>) expressions[0];
+        if (this.hasSection()) {
+            assert sectionNode != null;
+            this.loadOptionalCode(sectionNode);
+            if (last != null) last.setNext(null);
+        }
+        return true;
+    }
+    
+    @Override
+    public @NotNull String toString(@Nullable Event event, boolean debug) {
+        return "access file " + pathExpression.toString(event, debug);
+    }
+    
 }
