51b0831c1a3bc0847c7116e784b923ea9c2d0ad8
Moderocky <admin@moderocky.com>
2023-09-27 11:38:52 +0100

    Add doc extraction and test.

---
+++ src/test/java/org/skriptlang/skript_io/ExtractDocsInfoTest.java
@@ -0,0 +1,92 @@
+package org.skriptlang.skript_io;
+
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ExtractDocsInfoTest {
+    
+    public static void main(String[] args) {
+        final List<Element> elements = extractElements();
+        for (final Element element : elements) {
+            final Class<?> type = element.toClass();
+            assert type != null;
+            if (Modifier.isAbstract(type.getModifiers())) continue;
+            final Doc doc = Doc.of(type);
+            System.out.println();
+            System.out.println("#### " + doc.name);
+            System.out.println();
+            System.out.println("Since `" + doc.since + "`");
+            System.out.println();
+            for (final String s : doc.description) System.out.println(s);
+            System.out.println();
+            System.out.println("```sk");
+            for (final String example : doc.examples) System.out.println(example);
+            System.out.println("```");
+            System.out.println();
+        }
+    }
+    
+    @Test
+    public void extract() {
+        final List<Element> elements = extractElements();
+        assert !elements.isEmpty();
+        for (final Element element : elements) {
+            final Class<?> type = element.toClass();
+            assert type != null;
+            if (Modifier.isAbstract(type.getModifiers())) continue;
+            Doc.of(type);
+        }
+    }
+    
+    private static List<Element> extractElements() {
+        SkriptIO.testMode = true;
+        final String basePackage = "org.skriptlang.skript_io.elements", source = "src/main/java/org/skriptlang/skript_io/elements/";
+        final File root = new File(source);
+        assert root.exists() && root.isDirectory();
+        final List<Element> elements = new ArrayList<>();
+        for (final File file : Objects.requireNonNull(root.listFiles())) {
+            if (!file.isDirectory()) continue;
+            final String folder = file.getName();
+            for (final File listFile : Objects.requireNonNull(file.listFiles())) {
+                final String name = listFile.getName();
+                elements.add(new Element(basePackage, folder, name.substring(0, name.indexOf('.'))));
+            }
+        }
+        return elements;
+    }
+    
+    record Element(String base, String path, String name) {
+        
+        Class<?> toClass() {
+            try {
+                return Class.forName(base + "." + path + "." + name);
+            } catch (ClassNotFoundException e) {
+                return null;
+            }
+        }
+        
+    }
+    
+    record Doc(String name, String[] description, String[] examples, String since) {
+        
+        public static Doc of(Class<?> type) {
+            return new Doc(
+                type.getAnnotation(Name.class).value(),
+                type.getAnnotation(Description.class).value(),
+                type.getAnnotation(Examples.class).value(),
+                type.getAnnotation(Since.class).value()
+            );
+        }
+        
+    }
+    
+}
